4e89a3aef85b7e172582217a16ff7de15a49b260
Achim Grolimund <achim.grolimund@hf-ict.info>
2017-11-09 22:04:42 +0100

    Settings can now Write to an .ini File

--- settingsmanager.cpp
+++ settingsmanager.cpp
@@ -1,23 +1,32 @@
 #include "settingsmanager.h"
+#include "mainwindow.h"
 #include <QSettings>
+
 
 settingsManager::settingsManager()
 {
-	QSettings my_settings("C:/settings.ini", QSettings::IniFormat);
-}
-
-void settingsManager::loadSettings()
-{
 
 }
+
+
 
 void settingsManager::setUsername()
 {
-	QSettings settings;
+	MainWindow win;
+	// <-- (Filename.ini , Format as ini -->
+	QSettings settings("corporateSettings.ini", QSettings::IniFormat);
 
+	// <-- begin Writing in Group [UserDaten] -->
 	settings.beginGroup("UserDaten");
 	settings.setValue("username","Achim");
 	settings.endGroup();
+
+	// <-- new group [Window] -->
+	settings.beginGroup("Window");
+	settings.setValue("size", win.size());
+	settings.setValue("pos", win.pos());
+	settings.endGroup();
+
 	settings.sync();
 
 }
